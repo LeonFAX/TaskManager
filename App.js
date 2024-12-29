f03a41b4ca3a31cf32894fb279cda556e1b1e542
LeonFAX <98383909+LeonFAX@users.noreply.github.com>
2024-12-29 04:10:09 +0300

    Normalize line endings

--- App.js
+++ App.js
@@ -1,20 +1,52 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import React, { useEffect, useState } from 'react';
+import { View, StyleSheet, ScrollView } from 'react-native';
+import TaskForm from './TaskForm';
+import TaskList from './TaskList';
+import AsyncStorage from '@react-native-async-storage/async-storage';
 
-export default function App() {
+const App = () => {
+  const [tasks, setTasks] = useState([]);
+
+  const loadTasks = async () => {
+    const keys = await AsyncStorage.getAllKeys();
+    const taskPromises = keys.map(key => AsyncStorage.getItem(key));
+    const taskData = await Promise.all(taskPromises);
+    const loadedTasks = taskData.map(task => JSON.parse(task)).filter(Boolean);
+    setTasks(loadedTasks);
+  };
+
+  const addTask = (task) => {
+    setTasks([...tasks, task]);
+  };
+
+  const deleteTask = async (title) => {
+    await AsyncStorage.removeItem(`task_${title}`);
+    setTasks(tasks.filter(task => task.title !== title));
+  };
+
+  const updateTask = (title) => {
+  };
+
+  useEffect(() => {
+    loadTasks();
+  }, []);
+
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
+      <ScrollView>
+        <TaskForm onAddTask={addTask} />
+        <TaskList tasks={tasks} onDeleteTask={deleteTask} onUpdateTask={updateTask} />
+      </ScrollView>
     </View>
   );
-}
+};
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    padding: 10,
+    backgroundColor: '#f0f0f0',
   },
 });
+
+export default App;
